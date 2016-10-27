980bc57700ec19ecdc48f7cda66d73147c14149c
jkillz2020 <kilgore.josh@gmail.com>
2016-10-26 21:18:19 -0500

    object return from api on zipcode enter and writing info to DOM

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@
 
 let apiKeys = {};
 
-let imageList = (searchText) =>{
+let localForecast = (zipText) =>{
   return new Promise((resolve, reject) =>{
     $.ajax({
       method: 'GET',
@@ -10,20 +10,16 @@ let imageList = (searchText) =>{
     }).then(response =>{
       console.log('response', response);
       apiKeys = response;
-      let authHeader = "Client-ID " + apiKeys.client_id;
+      //let authHeader = "Client-ID " + apiKeys.client_id;
 
 
 $.ajax({
   method: 'GET',
-  headers:{
-    'Authorization': authHeader
-  },
-  url: `https://api.imgur.com/3/gallery/t/${searchText}`
+  url: `http://api.openweathermap.org/data/2.5/weather?zip=${zipText},us&&APPID=5ab34be0357b9aa13610f7a9816381a4`
 }).then( (response2)=>{
-  console.log("imgur response", response);
-  resolve(response2.data.items);
+  resolve(response2);
 }, (errorResponse2)=>{
-  console.log('imgur fail', errorResponse2);
+  console.log('weather fail', errorResponse2);
   reject(errorResponse2);
 }, (errorResponse)=>{
       console.log('errorResponse', errorResponse);
@@ -31,3 +27,20 @@ $.ajax({
   });
 });
 };
+
+$(document).ready(function(){
+  console.log("jquery ready");
+  $("#clicky-button").on('click', ()=>{
+    $("#clicky-button").button("loading");
+    $('#output').html("");
+    let searchy = $('#weather-search').val();
+    console.log("its working");
+    localForecast(searchy).then((dataFromApi)=>{
+      $("#clicky-button").button("reset");
+    console.log('dataFromApi', dataFromApi);
+      $('#output').append(`<div>${dataFromApi.weather[0].description}</div>`);
+  });
+
+  });
+});
+
